48b1e64c6ff7bab1467e09a47ea4813995e1bdba
Khoirul Malik <khoirulmalik03@gmail.com>
2025-11-25 14:02:17 +0700

    Add tech stack data with icons

---
+++ src/components/data/techStackData.js
@@ -0,0 +1,37 @@
+import {
+  FaJs,
+  FaPython,
+  FaHtml5,
+  FaReact,
+  FaNodeJs,
+  FaVuejs,
+  FaLinux,
+} from "react-icons/fa";
+import {
+  SiTypescript,
+  SiCplusplus,
+  SiExpress,
+  SiFlask,
+  SiMongodb,
+  SiPostgresql,
+} from "react-icons/si";
+import { HiServer, HiShieldCheck } from "react-icons/hi";
+import { MdNetworkWifi } from "react-icons/md";
+
+export const techStackData = [
+  { id: 1, name: "JavaScript", icon: FaJs },
+  { id: 2, name: "TypeScript", icon: SiTypescript },
+  { id: 3, name: "Python", icon: FaPython },
+  { id: 4, name: "C++", icon: SiCplusplus },
+  { id: 5, name: "HTML/CSS", icon: FaHtml5 },
+  { id: 6, name: "React.js", icon: FaReact },
+  { id: 7, name: "Node.js", icon: FaNodeJs },
+  { id: 8, name: "Express.js", icon: SiExpress },
+  { id: 9, name: "Vue.js", icon: FaVuejs },
+  { id: 10, name: "Flask", icon: SiFlask },
+  { id: 11, name: "MongoDB", icon: SiMongodb },
+  { id: 12, name: "PostgreSQL", icon: SiPostgresql },
+  { id: 13, name: "Linux", icon: FaLinux },
+  { id: 14, name: "Networking", icon: MdNetworkWifi },
+  { id: 15, name: "Cybersecurity", icon: HiShieldCheck },
+];
