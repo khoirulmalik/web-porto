64c5228e5cb961e709dcc3b93d63fea45ede87b4
Khoirul Malik <khoirulmalik03@gmail.com>
2025-11-25 14:12:26 +0700

    Add certifications data

---
+++ src/components/data/certificatesData.js
@@ -0,0 +1,68 @@
+import {
+  FaCloud,
+  FaNetworkWired,
+  FaRoute,
+  FaServer,
+  FaNodeJs,
+  FaDatabase,
+  FaPython,
+  FaHtml5,
+  FaMicrochip,
+} from "react-icons/fa";
+
+export const certificatesData = [
+  {
+    id: 1,
+    title: "HCIA-Cloud Computing V5.0",
+    issuer: "Huawei",
+    icon: FaCloud,
+  },
+  {
+    id: 2,
+    title: "HCIA-Datacom V1.0",
+    issuer: "Huawei",
+    icon: FaNetworkWired,
+  },
+  {
+    id: 3,
+    title: "CCNAv7: Switching, Routing, and Wireless Essentials",
+    issuer: "Cisco",
+    icon: FaRoute,
+  },
+  {
+    id: 4,
+    title: "Belajar RESTful dengan Express.js",
+    issuer: "CODEPOLITAN",
+    icon: FaServer,
+  },
+  {
+    id: 5,
+    title: "Belajar Dasar Node.js dan NPM",
+    issuer: "CODEPOLITAN",
+    icon: FaNodeJs,
+  },
+  {
+    id: 6,
+    title: "Belajar MongoDB",
+    issuer: "CODEPOLITAN",
+    icon: FaDatabase,
+  },
+  {
+    id: 7,
+    title: "Python Lanjutan (Gold)",
+    issuer: "Skilvul",
+    icon: FaPython,
+  },
+  {
+    id: 8,
+    title: "Belajar Dasar Pemrograman Web",
+    issuer: "Dicoding",
+    icon: FaHtml5,
+  },
+  {
+    id: 9,
+    title: "IoT Fundamentals: Connecting Things",
+    issuer: "Cisco",
+    icon: FaMicrochip,
+  },
+];
