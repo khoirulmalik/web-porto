b08809848cc7fcb09dd079dfa66e8473e427108e
Khoirul Malik <khoirulmalik03@gmail.com>
2025-11-25 14:12:21 +0700

    Add organization and leadership data

---
+++ src/components/data/organizationData.js
@@ -0,0 +1,28 @@
+import { FaTrophy, FaUsersCog, FaSitemap } from "react-icons/fa";
+
+export const organizationData = [
+  {
+    id: 1,
+    title: "National Networking Competition 2.0",
+    role: "Head of Event Division (Apr 2023 – Feb 2024)",
+    description:
+      "Led a national event featuring Networking & CyberSecurity training, competitions, and seminars. Managed over 220 teams and a prize pool exceeding IDR 41 million, reaching over 100,000 people.",
+    icon: FaTrophy,
+  },
+  {
+    id: 2,
+    title: "KMTEDI UGM",
+    role: "Staff of Interests and Talents (Jan 2023 – Jun 2024)",
+    description:
+      "Developed platforms for student talents (academic & non-academic). As Vice Project Officer for TGES 2024, I helped manage an event with 78 committee members and 360 participants.",
+    icon: FaUsersCog,
+  },
+  {
+    id: 3,
+    title: "FORKOM TRI UGM",
+    role: "Vice Head of Interests and Talents (Jan 2023 – Mar 2024)",
+    description:
+      "Planned and executed seven key programs for the division, including Cyber League 2023, successfully fostering student engagement and participation in various activities.",
+    icon: FaSitemap,
+  },
+];
